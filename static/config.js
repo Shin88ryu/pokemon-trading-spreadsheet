78a6848e7570e2bd5ae52a45e3a2ea3dc5fde6be
SilverDwarf <50753298+SilverDwarf@users.noreply.github.com>
2019-12-10 00:52:14 -0600

    Update config.js

--- static/config.js
+++ static/config.js
@@ -13,7 +13,7 @@ var inGameName = "Lynn";
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/SilverDwarf330";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
